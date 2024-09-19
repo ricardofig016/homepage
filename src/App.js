252bd3c0acfa51435ad0f53e7aed85fb17750002
ricardofig016 <ricardifigueiredo016@gmail.com>
2024-09-19 14:55:07 +0100

    make homepage a react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to my Github HomePage!</h1>
+      <a href="spotifree">SpotiFree</a>
+    </>
+  );
+}
+
+export default App;
