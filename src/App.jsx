be9068778a8c1e0eab4754661bc7e21afaee2f2e
Ricardo Figueiredo <ricardocastrofigueiredo@gmail.com>
2024-09-21 18:24:45 +0100

    fix base path in vite.config.js to solve build errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import Footer from './Footer.jsx'
-import Header from './Header.jsx'
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
 
 function App() {
-
   return (
     <>
       <Header />
 
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,21 @@
-
 function Header() {
   return (
     <header>
       <h1>My Homepage</h1>
       <nav>
         <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#news">News</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
         </ul>
       </nav>
     </header>
